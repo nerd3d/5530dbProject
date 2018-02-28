8a28d312135c10488a1527512a1b885b80884cc4
Christopher Allan <nerd3d@hotmail.com>
2018-02-28 13:51:52 -0700

    Wasted my time!

--- src/UUber/Connector.java
+++ src/UUber/Connector.java
@@ -8,6 +8,69 @@ import java.sql.Statement;
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
+/**
+ * Connector handles the connection to the database.
+ **/
 class Connector {
+	public Statement st = null; // used to prepare and issue statements to the database
+	public Connection con = null; // maintains the connection to the database
+	public Session sesh = null; // maintains the connection to the host
+	
+	/**
+	 * Instantiates a new Connector Object. Creates a connection to the host and
+	 * 	logs into the database
+	 **/
+	public Connector(String sshUser, String sshPassword) throws Exception {
+		String sshHost = "georgia.eng.utah.edu";
+		
+		int lport = 5656;
+		String rhost = "localhost";
+		int rport = 3306;
 
+		String url = "jdbc:mysql://localhost:" + lport + "/5530u21?allowMultiQueries=true";
+		String dbUser = "5530u21";
+		String dbPassword = "va9vg8h";
+		String driverName = "com.mysql.jdbc.Driver";
+		
+		try 
+		{
+			// modify configuration: remove strict host checking
+			java.util.Properties config = new java.util.Properties();
+			config.put("StrictHostKeyChecking", "no");
+			// create a new JSch object
+			JSch jsch = new JSch();
+			
+			// populate session field
+			sesh = jsch.getSession(sshUser, sshHost, 22);
+			sesh.setPassword(sshPassword);
+			sesh.setConfig(config);
+			sesh.connect(); // connect to host
+			System.out.println("Connected");
+			
+			// set port forwarding
+			int aport = sesh.setPortForwardingL(lport, rhost, rport);
+			System.out.println("localhost:" + aport + " -> " + rhost + ":" + rport);
+			System.out.println("Port Forwarded");
+			
+			// Connect to database
+			Class.forName(driverName).newInstance();
+			con = DriverManager.getConnection(url, dbUser, dbPassword);
+			st = con.createStatement();
+			
+			System.out.println("Database connection established");
+			System.out.println("DONE");
+		}
+		catch (SQLException sql)
+		{
+			sql.printStackTrace();
+			throw sql;
+		}
+		
+	}
+	
+	public void CloseConnection() throws Exception 
+	{
+		con.close();
+		sesh.disconnect();
+	}
 }
--- src/UUber/Driver.java
+++ src/UUber/Driver.java
@@ -5,9 +5,45 @@ import java.sql.*;
 import java.io.*;
 
 public class Driver {
-	
+
+	static Connector connect = null;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+
+		try {
+			ConnectToDB();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void ConnectToDB() throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String login;
+		String pass;
+		System.out.println("Welcome to UUber database!");
+		System.out.println("Please enter Host Login Name:");
+		while ((login = in.readLine()) == null && login.length() == 0);
+		System.out.println("Please enter Host Password:");
+		while ((pass = in.readLine()) == null && pass.length() == 0);
+
+		try {
+			connect = new Connector(login, pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (connect != null) {
+				try {
+					connect.CloseConnection();
+					System.out.println("Database connection terminated");
+				}
+
+				catch (Exception e) {
+					/* ignore close errors */ }
+			}
+		}
 	}
 }
