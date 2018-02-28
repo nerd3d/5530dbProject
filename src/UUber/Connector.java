ce0456436cce12d7a570786a9d45b6cc990c686b
Christopher Allan <nerd3d@hotmail.com>
2018-02-28 11:44:10 -0700

    Started adding some classes (empty for now)

---
+++ src/UUber/Connector.java
@@ -0,0 +1,13 @@
+package UUber;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+class Connector {
+
+}
--- src/UUber/Driver.java
+++ src/UUber/Driver.java
@@ -1,9 +1,13 @@
+package UUber;
 
-public class Dummy {
+import java.lang.*;
+import java.sql.*;
+import java.io.*;
 
+public class Driver {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 	}
-
 }
