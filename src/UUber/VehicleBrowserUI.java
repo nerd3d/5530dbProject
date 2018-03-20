c785e4c2170dfb4ce931fe57b4a385da6179252b
Nautilus5 <adambennion17@hotmail.com>
2018-03-20 17:44:15 -0600

    Added selection ability for VehicleBrowser.

--- src/UUber/VehicleBrowserUI.java
+++ src/UUber/VehicleBrowserUI.java
@@ -2,6 +2,8 @@ package UUber;
 
 import java.sql.*;
 import java.time.*;
+import java.util.ArrayList;
+import java.util.List;
 
 //asks for filters
 //asks for sorting
@@ -87,17 +89,34 @@ public class VehicleBrowserUI {
 		switch(caller) {
 		case "Ride":
 			//additional filter of only cars available right now
+			//selecting a car will give menu of
+			//record ride >> returns vin
+			//give feedback
+			//view feedback
+			//back to vehicle browser
+			//exit vehicle browser
 			break;
 		case "Reservation":
-			//?? any changes??
+			//any additional filter for reservation?
+			//selecting a car will give menu of
+			//set reservation >> returns vin
+			//give feedback
+			//view feedback
+			//back to vehicle browser
+			//exit vehicle browser
 			break;
 		case "MainMenu":
-			//no changes
+			//selecting a car will give menu of
+			//give feedback
+			//view feedback
+			//back to vehicle browser
+			//exit vehicle browser
 			break;
 		default:
 			return null;
 		}
 		while (true) {
+			List<String> vinList = new ArrayList<String>();
 			// welcome and list options...wait for input
 			//System.out.println("*** Vehicles ***");
 			System.out.println("Num\tVIN\tDriver");
@@ -112,13 +131,20 @@ public class VehicleBrowserUI {
 				System.out.print(row+"\t");
 				System.out.print(result.getString("vin")+"\t");
 				System.out.print(result.getString("name") + "\n");
-				
+				vinList.add(result.getString("vin"));
 				row++;
 			}
-			
-			System.out.println("\n<press any key to go back>"); // replace with selection options
-			Utils.getInput();
-			return null; // remove this when implemented!
+			System.out.println("Select a vehicle by the left-most number.");
+			int selected = Integer.parseInt(Utils.getInput());
+			if(selected < 1 || selected > vinList.size())
+			{
+				System.out.println("Invalid selection.");
+				return null;
+			}
+			return vinList.get(selected-1);
+			//System.out.println("\n<press any key to go back>"); // replace with selection options
+			//Utils.getInput();
+			//return null; // remove this when implemented!
 		}
 	}
 
