7215f273b95a05365d515a8327ed372fa45b2259
nerd3d <nerd3d@gmail.com>
2018-03-22 14:37:55 -0600

    Reservations are working. Removing and Adding.

--- src/UUber/ReservationUI.java
+++ src/UUber/ReservationUI.java
@@ -52,6 +52,14 @@ public class ReservationUI {
 			}
 
 			if (time != null) {
+				// make sure time isn't duplicated by this user
+				ResultSet existingRes = Utils.QueryHelper("SELECT * FROM Reservation WHERE login = '" + Utils.currentUser
+						+ "' AND time = '" + time.format(Utils.formatSQL).toString() + "';", Utils.stmt);
+				if(existingRes.next()) {
+					System.out.println("ERROR: You already have a reservation for this time slot");
+					return;
+				}
+
 				carResult = VehicleBrowserUI.ShowMenu("Reservation", time);
 				if (carResult == null) {
 					System.out.println("No valid Vehicle Chosen");
@@ -127,6 +135,8 @@ public class ReservationUI {
 					System.out.println("Aborting Cancel.. returning to main menu.");
 					return;
 				}
+				System.out.println("Please enter 'y' or 'n'");
+				yesorno = null;
 			}
 		} else {
 			System.out.println("ERROR: expecting integer within range");
@@ -136,27 +146,54 @@ public class ReservationUI {
 	/*
 	 * Attempt to Add a reservation to the database. Return true if successful
 	 */
-	private static boolean AddReservation(String vin, LocalDateTime time) {
-		// verify reservation Add
-		System.out.println("Are you Sure you want to add this reservation (y/n)?");
-		System.out.println("VIN: " + vin + "\nTime: " + time.format(Utils.formatSQL).toString());
-
-		String result = null;
-		while (result == null) {
-			try {
-				result = Utils.getInput();
-			} catch (Exception e) {
-				System.out.print("Please enter y or n: ");
-			}
-		}
-
-		if (result.equalsIgnoreCase("n")) {
-			System.out.println("Cancelling Reservation...");
+	private static boolean AddReservation(String vin, LocalDateTime time) throws Exception {
+		// Display reservation info for verification
+		String query = "SELECT * FROM Car C, Owns O WHERE C.vin = O.vin AND C.vin = '" + vin + "';";
+		ResultSet vehicle = Utils.QueryHelper(query, Utils.stmt);
+		if (!vehicle.next()) {
+			System.out.println("ERROR selecting vehicle from database");
 			return false;
 		}
 
+		System.out.println(vehicle.getString("login") + "\t" + vehicle.getString("model") + "\t"
+				+ time.format(Utils.formatSQL).toString());
+
+		// verify reservation Add
+		System.out.print("Are you Sure you want to add this reservation (y/n)? ");
+
+		String yesorno = null;
+		while (yesorno == null) {
+			yesorno = Utils.getInput();
+			if (yesorno.equalsIgnoreCase("y")) {
+				break;
+			}
+			if (yesorno.equalsIgnoreCase("n")) {
+				System.out.println("Aborting Reservation");
+				return false;
+			}
+			System.out.println("Please enter 'y' or 'n'");
+			yesorno = null;
+		}
+
 		/* Attempt to insert reservation. If Exception is caught, return false */
-		System.out.println("Not yet implemented");
+		query = "SELECT MAX(resid) FROM Reservation;";
+		ResultSet maxRes = Utils.QueryHelper(query, Utils.stmt);
+		int resid;
+		if (maxRes.next())
+			resid = maxRes.getInt(1) + 1;
+		else
+			resid = 1;
+
+		String addMe = "INSERT INTO Reservation VALUES ( ";
+		addMe += resid + ", '" + Utils.currentUser + "', '" + vin + "', '" + time.format(Utils.formatSQL).toString()
+				+ "');";
+
+		try {
+			Utils.UpdateHelper(addMe, Utils.stmt);
+		} catch (Exception e) {
+			System.out.println("Something went wrong while adding reservation");
+			return false;
+		}
 
 		return true;
 	}
