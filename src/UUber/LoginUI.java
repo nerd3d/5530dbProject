24450d4534d2d70bc9917820d31650b21f88db13
nerd3d <nerd3d@gmail.com>
2018-03-22 12:37:34 -0600

    Fixed state values

--- src/UUber/LoginUI.java
+++ src/UUber/LoginUI.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 public class LoginUI {
 	//grabbed an array of states of the web to restrict input to real states.
-static String[] states = {"california,", "alabama,", "arkansas,", "arizona,", "alaska,", "colorado,", "connecticut,", "delaware,", "florida,", "georgia,", "hawaii,", "idaho,", "illinois,", "indiana,", "iowa,", "kansas,", "kentucky,", "louisiana,", "maine,", "maryland,", "massachusetts,", "michigan,", "minnesota,", "mississippi,", "missouri,", "montana,", "nebraska,", "nevada,", "new Hampshire,", "new jersey,", "new mexico,", "new york,", "north carolina,", "north dakota,", "ohio,", "oklahoma,", "oregon,", "pennsylvania,", "rhode Island,", "south carolina,", "south dakota,", "tennessee,", "texas,", "utah,", "vermont,", "virginia,", "washington,", "west virginia,", "wisconsin,", "wyoming" };
+static String[] states = {"california", "alabama", "arkansas", "arizona", "alaska", "colorado", "connecticut", "delaware", "florida", "georgia", "hawaii", "idaho", "illinois", "indiana", "iowa", "kansas", "kentucky", "louisiana", "maine", "maryland", "massachusetts", "michigan", "minnesota", "mississippi", "missouri", "montana", "nebraska", "nevada", "new hampshire", "new jersey", "new mexico", "new york", "north carolina", "north dakota", "ohio", "oklahoma", "oregon", "pennsylvania", "rhode island", "south carolina", "south dakota", "tennessee", "texas", "utah", "vermont", "virginia", "washington", "west virginia", "wisconsin", "wyoming" };
 
 	public static void ShowMenu() throws Exception {
 		while (true) {
