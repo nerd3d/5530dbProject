076f2a2504b2456ab500c60cda2fb9c2736402e5
nerd3d <nerd3d@gmail.com>
2018-03-24 17:18:44 -0600

    Statistics-> Most Expensive Cars COMPLETED

--- src/UUber/StatisticsUI.java
+++ src/UUber/StatisticsUI.java
@@ -61,7 +61,7 @@ public class StatisticsUI {
 			String query = "SELECT login, year, make, model, rides " + "FROM Car C, Owns O, "
 					+ "(SELECT vin, COUNT(vin) rides FROM Ride GROUP BY vin) R "
 					+ "WHERE C.vin = O.vin AND C.vin = R.vin " + "AND category = '" + cat + "'"
-					+ "ORDER BY rides LIMIT " + limit + ";";
+					+ "ORDER BY rides DESC LIMIT " + limit + ";";
 			result = Utils.QueryHelper(query, Utils.stmt);
 
 			while (result.next()) {
@@ -73,20 +73,63 @@ public class StatisticsUI {
 				
 				System.out.println(disp);
 			}
-			System.out.println("--------------------------------------------------------");
+			System.out.println("--------------------------------------------------------\n");
 		}
 		
 		System.out.println("<Press Enter to Return>");
 		Utils.getInput();
 	}
 
-	private static void ExpensiveCars() {
-		// TODO Auto-generated method stub
+	private static void ExpensiveCars() throws Exception {
+		System.out.print("Limit results per category to (default 5): ");
+		int limit = 5;
+		boolean valid = false;
+		while (!valid) {
+			try {
+				String lim = Utils.getInput();
+				if (lim.isEmpty())
+					break;
+				limit = Integer.parseInt(lim);
+				valid = true;
+			} catch (Exception e) {
+				System.out.println("ERROR: Please enter a single integer");
+			}
+		}
 
+		ArrayList<String> categories = new ArrayList<String>();
+		ResultSet result = Utils.QueryHelper("SELECT * FROM Category;", Utils.stmt);
+		while (result.next()) {
+			categories.add(result.getString("category"));
+		}
+
+		for (String cat : categories) {
+			System.out.println("Category = " + cat);
+			System.out.println("Driver\tYear\tMake\tModel\tAverage Cost");
+			System.out.println("--------------------------------------------------------");
+			String query = "SELECT login, year, make, model, average " + "FROM Car C, Owns O, "
+					+ "(SELECT vin, AVG(cost) average FROM Ride GROUP BY vin) R "
+					+ "WHERE C.vin = O.vin AND C.vin = R.vin " + "AND category = '" + cat + "'"
+					+ "ORDER BY average DESC LIMIT " + limit + ";";
+			result = Utils.QueryHelper(query, Utils.stmt);
+
+			while (result.next()) {
+				String disp = result.getString("login") + "\t";
+				disp += result.getString("year") + "\t";
+				disp += result.getString("make") + "\t";
+				disp += result.getString("model") + "\t";
+				disp += result.getString("average");
+				
+				System.out.println(disp);
+			}
+			System.out.println("--------------------------------------------------------\n");
+		}
+		
+		System.out.println("<Press Enter to Return>");
+		Utils.getInput();
 	}
 
 	private static void PopularDrivers() {
-		// TODO Auto-generated method stub
+		
 
 	}
 }
